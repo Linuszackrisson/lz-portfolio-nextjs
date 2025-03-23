92f46c807492b51ba287b106596521af7ff18b5a
Linuszackrisson <linus.gz@hotmail.com>
2025-03-23 22:52:14 +0100

    Korrekta länkar

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,32 +9,32 @@ const Projects = () => {
             description: "En plattform där användare kan hitta gratis alternativ till kostsam programvara, bläddra bland kategorier och betygsätta olika program. Genom projektet fick jag en djupare förståelse för fullstack-utveckling, inklusive databashantering, backend-logik och frontend-design. ",
             image: "/morganfreeware.png",
             technologies: ["Next.js", "TypeScript", "Tailwind", "Supabase"],
-            previewLink: "https://example.com/preview",
-            githubLink: "https://github.com/yourusername/lz-web"
+            previewLink: "https://morgan-freeware-22vyau2ko-linuszackrissons-projects.vercel.app/",
+            githubLink: "https://github.com/Linuszackrisson/morgan-freeware"
         },
         {
             title: "PickMyPlate",
             description: "En interaktiv app som inspirerar matlagning genom att föreslå recept från ett externt API. Användare väljer matkategori eller får slumpmässiga förslag, och utforskar detaljerade receptinstruktioner, ingredienslistor och videotutorials.",
             image: "/mealmock.png",
             technologies: ["HTML", "Tailwind", "JavaScript", "React"],
-            previewLink: "https://example.com/preview",
-            githubLink: "https://github.com/yourusername/pickmyplate"
+            previewLink: "https://linuszackrisson.github.io/PickMyPlate/",
+            githubLink: "https://github.com/Linuszackrisson/PickMyPlate"
         },
         {
             title: "Anslagstavla-App",
             description: "En fullstack-applikation för en digital anslagstavla där användare kan posta, ändra, ta bort och filtrera meddelanden efter användare och datum. Frontend byggd med React och deployad på AWS S3. Backend använder Serverless Framework med API Gateway, Lambda och DynamoDB.",
             image: "/anslag.png",
             technologies: ["React", "CSS", "JavaScript", "AWS"],
-            previewLink: "https://example.com/preview",
-            githubLink: "https://github.com/yourusername/anslagstavla"
+            previewLink: "https://anslagstavlan-app.s3.eu-north-1.amazonaws.com/index.html",
+            githubLink: "https://github.com/Linuszackrisson/anslagstavla-app"
         },
         {
-            title: "LZ Web Landing Page",
+            title: "Lzweb.se",
             description: "En landing page för en just nu fiktiv webbyrå i Arvika, skapad för att få en djupare förståelse för TailwindCSS och Typescript, med extra fokus på design och användarupplevelse.",
             image: "/lzweb.png",
             technologies: ["React", "TypeScript", "Tailwind"],
-            previewLink: "https://example.com/preview",
-            githubLink: "https://github.com/yourusername/lz-web"
+            previewLink: "https://linuszackrisson.github.io/lzweb-landingpage/",
+            githubLink: "https://github.com/Linuszackrisson/lzweb-landingpage"
         }
     ];
 
